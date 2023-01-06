e176cc7c57c481c445ce999e13ab88b5249afd22
Денис Булавский <denis.bulawskiy@gmail.com>
2023-01-06 20:47:29 +0300

    Добавлены размеры окна и кнопка

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+root = Tk()
+#root.mainloop()
+# Размеры окна
+WIDTH = 640
+HEIGHT = 480
+POS_X = root.winfo_screenwidth() // 2 - WIDTH // 2
+POS_Y = root.winfo_screenwidth() // 2 - HEIGHT
+#Устанавливаем ширину, высоту и позицию
+root.geometry(f"{WIDTH}x{HEIGHT}+{POS_X}+{POS_Y}")
+#Заголовок окна
+root.title("Тест окна")
+#Запрещаем изменение размеров окна
+root.resizable(False, False)
+#кнопка
+button01 = Button()
+button01["text"] = "Button 1"
+X_BTN = WIDTH // 2 - button01.winfo_reqwidth() // 2
+Y_BTN = HEIGHT // 2 - button01.winfo_reqheight() // 2
+button01.place(x=X_BTN, y=Y_BTN)
+button01["command"] = quit
+root.mainloop()
